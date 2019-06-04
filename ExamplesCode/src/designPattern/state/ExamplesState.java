d0f74009b8a8423210a42fb8d62f76b830ac8525
RHP <raulhperez@gmail.com>
2019-06-04 10:38:28 -0400

    change state

--- ExamplesCode/src/designPattern/state/ExamplesState.java
+++ ExamplesCode/src/designPattern/state/ExamplesState.java
@@ -4,8 +4,10 @@ public class ExamplesState {
 
 	public static void main(String[] args) {
 		Radio radio = new Radio(new OffRadioState()); //initial status
+		
 		radio.setState(new OnRadioState());
 		radio.execute(); //radio on
+		
 		radio.setState(new OffRadioState());
 		radio.execute(); //radio off
 
