a824aaf2fc327a9a874a2eb98118dd9c44f1bb45
RHP <raulhperez@gmail.com>
2020-02-13 08:33:00 -0500

    l i left trax company

--- ExamplesCode/src/testCode/Labelary.java
+++ ExamplesCode/src/testCode/Labelary.java
@@ -1,29 +1,24 @@
 package testCode;
 
 import java.lang.Object;
-import javax.xml.ws.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import javax.xml.ws.*;
 
 public class Labelary {
 
-	/*/// converts but upside down
-	String zpl = "^xa^cfa,50^fo100,100^fdHello World^fs^xz";
-
-	Client client = ClientBuilder.newBuilder().register(MultiPartFeature.class).build();
-	// adjust print density (8dpmm), label width (4 inches), label height (6 inches), and label index (0) as necessary
-	WebTarget target = client.target("http://api.labelary.com/v1/printers/8dpmm/labels/4x6/0/");
-	Invocation.Builder request = target.request();
-	request.accept("application/pdf"); // omit this line to get PNG images back
-	Response response = request.post(Entity.entity(zpl, MediaType.APPLICATION_FORM_URLENCODED));
-
-	if (response.getStatus() == 200) {
-	    byte[] body = response.readEntity(byte[].class);
-	    File file = new File("label.pdf"); // change file name for PNG images
-	    Files.write(file.toPath(), body);
-	} else {
-	    String body = response.readEntity(String.class);
-	    System.out.println("Error: " + body);
-	}	
-*/	
+	private Collection<String> list =  Arrays.asList("abC", "Long Mixed", "SHORT");;
 	
+	
+	List<String> list2 = list.stream().map(str -> str.length() > 5 ? str.toUpperCase() : str.toLowerCase()).collect(Collectors.toList());
+
+	/* /// converts but upside down String zpl = "^xa^cfa,50^fo100,100^fdHello World^fs^xz"; Client client = ClientBuilder.newBuilder().register(MultiPartFeature.class).build(); // adjust print
+	 * density (8dpmm), label width (4 inches), label height (6 inches), and label index (0) as necessary WebTarget target = client.target("http://api.labelary.com/v1/printers/8dpmm/labels/4x6/0/");
+	 * Invocation.Builder request = target.request(); request.accept("application/pdf"); // omit this line to get PNG images back Response response = request.post(Entity.entity(zpl,
+	 * MediaType.APPLICATION_FORM_URLENCODED)); if (response.getStatus() == 200) { byte[] body = response.readEntity(byte[].class); File file = new File("label.pdf"); // change file name for PNG
+	 * images Files.write(file.toPath(), body); } else { String body = response.readEntity(String.class); System.out.println("Error: " + body); } */
+
 }
